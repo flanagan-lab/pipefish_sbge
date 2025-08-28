0a5e96832fc930abedde3ddd364aa4045238a378
Sarah Flanagan <sarah.flanagan@canterbury.ac.nz>
2025-08-29 09:24:17 +1200

    creating a script to run prank for multiple sequence alignment of the single copy orthologs

---
+++ syngnathus/syngnathus_fuscus/bash/prank.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# paths relative to location of script
+FASTA_DIR="../OrthoFinder/Results_Aug15/Single_Copy_Orthologue_Sequences"
+SPP_TREE="../OrthoFinder/Results_Aug15/Species_Tree/SpeciesTree_rooted.txt"
+OUT_DIR="../OrthoFinder/Results_Aug15/Single_Copy_Orthologue_Alignments"
+
+# move to location of script
+cd "${0%/*}" 
+
+# check if the output directory does not exist
+if [ ! -d "$OUT_DIR" ]; then
+  mkdir $OUT_DIR # make the dir if it doesn't already exist
+fi
+
+for fa in $FASTA_DIR/*fa
+do
+	ID=$(basename $fa .fa) # remove the extension
+	echo "prank -d=$fa -t=$SPP_TREE -o=$OUT_DIR/$ID"
+done
+
